6baa1f665708af8a2a9767a8e75b8703c4a4b05d
ibracken <ian.d.bracken@gmail.com>
2025-09-20 17:10:02 -0400

    update cleanup

--- cleanup.py
+++ cleanup.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+# Set pandas to show all columns
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+
 # Import SpaceData.csv, skip comment lines starting with #
 df = pd.read_csv('SpaceData.csv', comment='#')
 
@@ -8,9 +12,12 @@ print(f"Dataset shape: {df.shape}")
 
 
 df = df[df["default_flag"] != 0]
-print(len(df))
-print(f"Temp Count: {df['st_teff'].head()}")
+
 # Planet Name, Host Name, Number of suns, Number of Planets, Year Discovered, Planet Orbital Period, Sun Temperature, Eccentricity, RA, Dec, Gaia Magnitude
 df = df[["pl_name", "hostname", "sy_snum", "sy_pnum", "disc_year", "pl_orbper", "st_teff", "pl_orbeccen", "ra", "dec", "sy_gaiamag"]]
 
+
+
+# Fill null eccentricity values with 0.01
+df['pl_orbeccen'] = df['pl_orbeccen'].fillna(0.1)
 print(df.head())
